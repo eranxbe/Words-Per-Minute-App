8859d28a2a8d1165f8a970b82fd86ceee2021c9f
eranxbe <79172040+eranxbe@users.noreply.github.com>
2022-12-30 13:04:59 +0200

    Create random-words.js

---
+++ random-words.js
@@ -0,0 +1,14 @@
+export let words = {
+    "words" : ["issue", "rob", "dependence", "angle", "hole", "frank", "application", "delicate",
+    "committee", "minister", "acquit", "student", "ceremony", "panel", "trunk", "symbol",
+    "receipt", "nursery", "husband", "sniff", "seize", "growth", "mature", "ignorant",
+    "central", "elaborate", "short", "essay", "harmful", "pigeon", "standard", "learn",
+    "orientation", "sugar", "speaker", "stadium", "dance", "suspect", "oppose", "desk",
+    "weigh", "mystery", "launch", "raw", "undertake", "triangle", "tune", "weak", "psychology",
+    "miracle", "keep", "amber", "tough", "terrify", "loan", "circulate", "coup", "television",
+    "swarm", "pocket", "hardware", "weapon", "absent", "favorable", "shiver", "bare", "computer",
+    "prestige", "last", "ordinary", "chew", "affinity", "dragon", "shaft", "pace", "mile",
+    "loss", "accent", "mass", "memorial", "crutch", "thirsty", "advertising", "middle", "survival",
+    "obscure", "haunt", "cupboard", "cable", "bold", "demonstration", "absorption", "sodium",
+    "mirror", "combine", "soil", "coverage", "thesis", "temptation", "great"]
+}
